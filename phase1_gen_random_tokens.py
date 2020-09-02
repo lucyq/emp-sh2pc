d8064608024ceb8f884cc97c9565d636f9204067
Lucy Qin <lucyzqin@gmail.com>
2020-09-02 14:03:36 -0400

    Delete phase1_gen_random_tokens.py

--- phase1_gen_random_tokens.py
+++
@@ -1,8 +0,0 @@
-import os,codecs
-
-num_rows = 100
-FILE_NAME = "phase1_test_tokens.txt"
-with open(FILE_NAME, "w") as f:
-	for _ in range(num_rows):
-		myhex = codecs.encode(os.urandom(96), 'hex').decode()
-		f.write(myhex + "\n")
